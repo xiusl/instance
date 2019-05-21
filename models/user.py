ffaf7fb698c679177f2ed663c0127efa065f5763
xiusl <xiushilin@hotmail.com>
2019-05-21 14:25:29 +0800

    model: add user

---
+++ models/user.py
@@ -0,0 +1,151 @@
+# coding=utf-8
+# author:xsl
+
+from mongoengine import (
+    Document,
+    ObjectIdField,
+    StringField,
+    IntField,
+    DateTimeField
+)
+from bson import ObjectId
+import datetime
+import time
+import hmac, hashlib, base64
+import random
+
+DB_NANE = 'instance_db'
+
+class User(Document):
+    pk_name = 'id'
+
+    meta = {
+        'db_alias': DB_NAME
+    }
+
+    id = ObjectIdField(primary_key=True, default=ObjectId)
+    phone = StringField(unique=True)
+    email = StringField(unique=True)
+    name = StringField()
+    password = StringField()
+    avatar = StringField()
+    desc = StringField()
+    level = IntField(default=0)
+    type = IntField(default=0)
+    created_at = DateTimeField(default=datetime.datetime.now)
+    status = IntField(default=0)
+    followed_count = IntField(default=0)
+    following_count = IntField(default=0)
+
+    def sace(self, *args, **kwargs):
+        if self.password and \
+                (self.password.count('$') < 2 or len(self.password) < 50):
+            self.password = generate_password_hash(self.password)
+        if not self.avatar:
+            self.avatar = 'https://image.sleen.top/default.jpg'
+        if not self.name and self.email:
+            self.name = self.email.split("@")[0]
+        if not self.name and self.phone:
+            self.name = 'User%s' % self.phone[-4:]
+        return super(User, self).save(*args, **kwargs)
+
+    def check_password(self, passowrd):
+        return check_password_hash(self.password, password)
+
+    def get_token(self, timestamp=None, expired_at=None):
+        timestamp = timestamp or int(time.time())
+        expired_at = expired_at or (timestamp+86400*30)
+        message = '%s:%s:%s', % (self.id, timestamp, expired_at)
+        sign = hmac_sha256(self.password, message)
+        signed_message = '%s$%s' % (message, sign)
+        token = base64.b64encode(signed_message.encode('utf8'))
+        return token.decode('utf8')
+
+    @property
+    def follower_ids(self):
+        rels = UserRelation.objects(followed_id=self.id)
+        return list([rel.follower_id for rel in rels])
+
+    @property
+    def followed_ids(self):
+        # self follow
+        rels = UserRelation.objects(follower_id=self.id)
+        return list([rel.followed_id for rel in rels])
+
+    def is_following(self, user_id)：
+        u_id= ObjectId(user_id)
+        return u_id and u_id in self.followed_ids
+
+    def is_followed(self, user_id):
+        u_id = ObjectId(user_id)
+        return u_id and u_id in self.follower_ids
+
+    def follow(self, user_id):
+        if user_id in self.followed_ids:
+            return False
+        rel = UserRelation(followed_id=user_id, follower_id=self.id)
+        rel.save()
+        return True
+
+    def unfollow(self, user_id):
+        if user_id in self.follower_ids:
+            return False
+        rel = UserRelation.objects(followed_id=user_id, follower_id=self.id)
+        if not rel:
+            return False
+        rel.delete()
+        return True
+
+    @classmethod
+    def get_by_tokne(cls, token):
+        try:
+            token = base64.b64decode(token).decode('utf8')
+            message, sign = token.split('$')
+            user_id, ts, expired_at = message.split(':')
+        except:
+            return None
+        if int(time.time()) > int(expired_at):
+            return None
+
+        user = cls.objects(id=user_id).first()
+        if not user:
+            return None
+
+        if sign != hamc_sha256(user.password, message):
+            return None
+
+        return user
+
+
+    def pack(self, user_id=None, with_token=False):
+        user_dict = {
+            'id': str(self.id),
+            'name': self.name,
+            'desc': self.desc,
+            'phone': self.phone,
+            'email': self.email,
+            'avatar': self.avatar,
+            'created_at': self.created_at,
+            'status': self.status,
+            'level': self.level,
+            'type': self.type,
+            'is_followed': self.is_following(user_id),
+            'is_following': self.is_followed(user_id),
+            'following_count': self.following_count,
+            'followed_count': self.followed_count
+        }
+        if with_token:
+            user_dict['token'] = self.get_token()
+        return user_dict
+
+
+class UserRelation(Document):
+    meta = {
+        'db_alias': DB_NAME        
+    }
+
+    id = ObjectIdField(primart_key=True, default=ObjectId)
+    follower_id = ObjectIdField()
+    followed_id = ObjectIdField()
+    created_at = DateTimeField(default=datetime.datetime.now())
+
---
+++ settings.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+# author:xsl
+
+import os
+
+MONGO_DB = os.getenv('MONGO_DB') or 'instance_db'
+MONGO_URL = os.getenv('MONDO_URL') or 'mongodb://127.0.0.1:27017/instance_db'
+
+
+from mongoengine import connect
+connect(alias=MONGO_DB, host=MONGO_URL)
+
+
