658da80a126bf781677b2f2395586ae926f999b9
xiusl <xiushilin@hotmail.com>
2019-07-05 16:04:48 +0800

    proj: add readme

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask, g, current_app, request, make_response
 from flask_restful import Api
 from flask_cors import CORS
 
-import settings
+#import settings
 from instance.models import User
 from instance.utils import output_json
 from instance.resource import (
